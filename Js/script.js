0a8ce7eb10edca3b391dc69dbf3b4724ba038698
sabrintsya <sabrinatasya170306@gmail.com>
2024-01-03 11:53:05 +0700

    first push

--- Js/script.js
+++ Js/script.js
@@ -6,45 +6,47 @@ const navigation = document.querySelector('.navigation');
 hamburger.addEventListener('click', () => {
     navigation.classList.toggle('hide');
 });
-
-/* hitung luas segitiga */
-function cek(){
-    if(form.alas.value=="" || form.tinggi.value==""){
-    alert("angka kosong");
-    exit
+function cekLuas() {
+        var form = document.forms["luasForm"];
+        if (form.alas.value == "" || form.tinggi.value == "") {
+            alert("Angka kosong");
+            return false;
+        }
+        return true;
     }
-  }
-  function hitung() {
-    cek();
-    a=parseInt(form.alas.value);
-    t=parseInt(form.tinggi.value);
-    form.hasil.value=a*t/2;
-  }
 
-  /* hitung keliling segitiga */
-  function cek() {
-    var Sisi1 = document.getElementById('Sisi1').value;
-    var Sisi2 = document.getElementById('Sisi2').value;
-    var Sisi3 = document.getElementById('Sisi3').value;
-
-    if (Sisi1 === "" || Sisi2 === "" || Sisi3 === "") {
-        alert("Angka kosong");
-        return false;
+    function hitungLuas() {
+        if (cekLuas()) {
+            var form = document.forms["luasForm"];
+            var a = parseFloat(form.alas.value);
+            var t = parseFloat(form.tinggi.value);
+            form.luasHasil.value = (a * t) / 2;
+        }
     }
-    return true;
-}
 
-function hitung() {
-    if (cek()) {
-        var S1 = parseFloat(document.getElementById('Sisi1').value);
-        var S2 = parseFloat(document.getElementById('Sisi2').value);
-        var S3 = parseFloat(document.getElementById('Sisi3').value);
-        var keliling = S1 + S2 + S3;
-        document.getElementById('hasil').value = keliling;
+    function cekKeliling() {
+        var Sisi1 = document.getElementById('Sisi1').value;
+        var Sisi2 = document.getElementById('Sisi2').value;
+        var Sisi3 = document.getElementById('Sisi3').value;
+
+        if (Sisi1 === "" || Sisi2 === "" || Sisi3 === "") {
+            alert("Angka kosong");
+            return false;
+        }
+        return true;
     }
-}
 
-function resetForm() {
-    document.getElementById('form1').reset();
-    document.getElementById('hasil').value = "";
-}
+    function hitungKeliling() {
+        if (cekKeliling()) {
+            var S1 = parseFloat(document.getElementById('Sisi1').value);
+            var S2 = parseFloat(document.getElementById('Sisi2').value);
+            var S3 = parseFloat(document.getElementById('Sisi3').value);
+            var keliling = S1 + S2 + S3;
+            document.getElementById('kelilingHasil').value = keliling;
+        }
+    }
+
+    function resetKelilingForm() {
+        document.getElementById('kelilingForm').reset();
+        document.getElementById('kelilingHasil').value = "";
+    }
